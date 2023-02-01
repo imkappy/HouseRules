49a7f8d84d3b274463a48ba063182de9a34432df
Caleb Caplinger <kidkappy@gmail,com>
2023-01-31 19:29:49 -0500

    Added Main Menu and return
    Main menu added
    Button to return to main menu added

---
+++ CardGames/Assets/Scripts/Scene_Manager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_Manager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Solitaire", LoadSceneMode.Single);
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+}
